7e230aca300a917608e4f53bde1c92f414611750
briandav22 <briandav22@gmail.com>
2019-03-12 14:17:56 -0400

    adding readme

---
+++ README.md
@@ -0,0 +1,7 @@
+
+Documentation for this wrapper can be found here: 
+
+https://api-docs2.readthedocs.io/en/latest/api_welcome.html
+
+
+I have also included a sample script to help get started. 
--- api_call_example.py
+++ api_call_example.py
@@ -4,12 +4,11 @@ from scrut_api import *
 
 # initiate Scurtinizer client. 
 client = scrut_api_client(
-         hostname="scrutinizer.plxr.local", 
-         authToken="BI5NGMrqd3ZsAvWPHekHmPUT")
+         hostname="some_scrutinizer_ip", 
+         authToken="some_auth_token")
 #set up report JSON. 
 
-report_object = scrut_json(filters = {"sdfDips_0": "in_10.1.1.252_ALL"},
-							reportTypeLang = "applications")
+report_object = scrut_json()
 report_format = scrut_data_requested()
 
 #load up params to be passed to request
@@ -20,15 +19,8 @@ params = scrut_params(
 
 )
 
-# make request. Data comes with .data attribute
-# response = scrut_request(params)
-
 response = scrut_request(params)
 
 
 #print out the data formatted. 
 scrut_print(response.data)
-
-
-# with open('data.json', 'w') as outfile:  
-#     json.dump(response.data, outfile)
